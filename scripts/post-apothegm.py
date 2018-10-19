4e902fb80b7fcf2b67e0fc4d2396c5fa64a181b1
Tim Schwab <tschwab@g.clemson.edu>
2018-10-20 04:15:36 +1100

    Put into functions

--- scripts/post-apothegm.py
+++ scripts/post-apothegm.py
@@ -10,25 +10,41 @@ from requests_oauthlib import OAuth1
 maxPostAttempts = 100
 historySize = 600
 
-# Load data
-with open('/home/pyzaist/auto-tweeter/data/apothegms.json') as f:
-	apothegms = json.load(f)
+def main():
+	# Load data
+	with open('/home/pyzaist/auto-tweeter/data/apothegms.json') as f:
+		apothegms = json.load(f)
+	
+	with open('/home/pyzaist/auto-tweeter/data/upcoming.json') as f:
+		upcoming = json.load(f)
 
-with open('/home/pyzaist/auto-tweeter/data/hashtags.json') as f:
-	hashtagList = json.load(f)
+	with open('/home/pyzaist/auto-tweeter/data/history.json') as f:
+		history = json.load(f)
+	
+	with open('/home/pyzaist/auto-tweeter/data/auth.json') as f:
+		auth = json.load(f)
+	
+	with open('/home/pyzaist/auto-tweeter/data/hashtags.json') as f:
+		hashtagList = json.load(f)
+	
+	# Post random apothegm, trying until it succeeds
+	for i in range(maxPostAttempts):
+		status = generateStatus(apothegms, upcoming, history, hashtagList)
+		if status == False:
+			continue
 
-with open('/home/pyzaist/auto-tweeter/data/history.json') as f:
-	history = json.load(f)
+		if postStatus(history, auth, status):
+			break
+	
+	# Save changes to data
+	with open('/home/pyzaist/auto-tweeter/data/history.json', 'w') as f:
+		f.write(json.dumps(history, indent=4))
+	
+	with open('/home/pyzaist/auto-tweeter/data/upcoming.json', 'w') as f:
+		f.write(json.dumps(upcoming, indent=4))
 
-with open('/home/pyzaist/auto-tweeter/data/auth.json') as f:
-	auth = json.load(f)
-
-with open('/home/pyzaist/auto-tweeter/data/upcoming.json') as f:
-	upcoming = json.load(f)
-
-# Post random apothegm, trying until it succeeds
-for i in range(maxPostAttempts):
-
+# Functions
+def generateStatus(apothegms, upcoming, history, hashtagList):
 	# If no upcoming ones with hashtags
 	if len(upcoming) == 0:
 		# Choose apothegm
@@ -38,21 +54,22 @@ for i in range(maxPostAttempts):
 		# Choose hashtags
 		if isinstance(apothegm, basestring):
 			hashtags = random.sample(hashtagList, 3)
+			return {'apothegm': apothegm, 'hashtags': hashtags}
 		else:
-			hashtags = apothegm['hashtags']
-			apothegm = apothegm['apothegm']
+			return apothegm
 
 		# Make sure it hasn't been posted recently
 		if apothegm in history:
 			print('Recently posted ' + apothegm + '. Searching again.')
-			continue
+			return False
 	
 	# If there are upcoming ones that I have added custom hashtags to
 	else:
-		apothegm = upcoming.pop(0)
-		hashtags = apothegm['hashtags']
-		apothegm = apothegm['apothegm']
+		return upcoming.pop(0)
 
+def postStatus(history, auth, status):
+	apothegm = status['apothegm']
+	hashtags = status['hashtags']
 	status = apothegm + '\n\n#' + hashtags[0] + '\n#' + hashtags[1] + '\n#' + hashtags[2]
 
 	# Post apothegm
@@ -72,15 +89,11 @@ for i in range(maxPostAttempts):
 		history.append(apothegm)
 		if len(history) > historySize:
 			history.pop(0)	
-		break
+		return True
 	else:
 		print('Error! Twitter didn\'t like that. Failed with status code ' + str(res.status_code))
 		print(res.text)
 		print('')
+		return False
 
-# Save changes
-with open('/home/pyzaist/auto-tweeter/data/history.json', 'w') as f:
-	f.write(json.dumps(history, indent=4))
-
-with open('/home/pyzaist/auto-tweeter/data/upcoming.json', 'w') as f:
-	f.write(json.dumps(upcoming, indent=4))
+main()
